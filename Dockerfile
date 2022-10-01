9624babae14c2cdcb2aa4296d565deecb8d3511c
spartakbelovfloresku@gmail.com <spartakbelovfloresku@gmail.com>
2022-10-01 12:21:14 -0500

    add commands to docker

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,5 @@
 FROM python:3.9-alpine3.13
-LABEL maintainer="django-api-project.com"
+LABEL maintainer="django-api-project"
 
 ENV PYTHONUNBUFFERED 1
 
@@ -16,7 +16,7 @@ RUN python -m venv /py && \
     apk add --update --no-cache --virtual .tmp-build-deps \
         build-base postgresql-dev musl-dev && \
     /py/bin/pip install -r /tmp/requirements.txt && \
-    if [ $DEV = "true" ]; \
+    if [ $DEV="true" ]; \
         then /py/bin/pip install -r /tmp/requirements.dev.txt ; \
     fi && \
     rm -rf /tmp && \
--- backend/base/management/commands/wait_for_db.py
+++ backend/base/management/commands/wait_for_db.py
@@ -1,5 +1,5 @@
 """
-Django command to wait for the db to be available.
+Django command to wait for the database to be available.
 """
 import time
 
@@ -10,17 +10,18 @@ from django.core.management.base import BaseCommand
 
 
 class Command(BaseCommand):
-    """Django commnad to wait for db"""
+    """Django command to wait for database."""
 
     def handle(self, *args, **options):
-        self.stdout.write('Wating fo DB...')
+        """Entrypoint for command."""
+        self.stdout.write('Waiting for database...')
         db_up = False
-        while not db_up:
+        while db_up is False:
             try:
                 self.check(databases=['default'])
                 db_up = True
-            except(Psycopg2OpError, OperationalError):
-                self.stdout.write('Database unavailable, waiting 1 sec...')
+            except (Psycopg2OpError, OperationalError):
+                self.stdout.write('Database unavailable, waiting 1 second...')
                 time.sleep(1)
 
         self.stdout.write(self.style.SUCCESS('Database available!'))
