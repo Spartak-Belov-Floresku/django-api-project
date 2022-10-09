0ba1ab97207e4229b4b7f8deb1b012ce6723e9db
spartakbelovfloresku@gmail.com <spartakbelovfloresku@gmail.com>
2022-10-08 22:19:15 -0500

    created recipe api added test and functionality

--- backend/app/settings.py
+++ backend/app/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'drf_spectacular',
     'base',
     'user',
+    'recipe',
 ]
 
 MIDDLEWARE = [
--- backend/base/tests/test_models.py
+++ backend/base/tests/test_models.py
@@ -1,8 +1,26 @@
 """
 Tests for models.
 """
+from decimal import Decimal
+
 from django.test import TestCase
 from django.contrib.auth import get_user_model
+
+from base import models
+
+
+def create_user(admin=False, **params):
+    """Create and return a new user."""
+    defaults = {
+        'email': 'test@mail.com',
+        'password': 'password123',
+    }
+    defaults.update(params)
+
+    if not admin:
+        return get_user_model().objects.create_user(**defaults)
+    else:
+        return get_user_model().objects.create_superuser(**defaults)
 
 
 class ModelTests(TestCase):
@@ -12,10 +30,7 @@ class ModelTests(TestCase):
         """Test creating a user wiyth an email is successful."""
         email = 'test@mail.com'
         password = 'testpass123'
-        user = get_user_model().objects.create_user(
-            email=email,
-            password=password,
-        )
+        user = create_user(email=email, password=password)
 
         self.assertEquals(user.email, email)
         self.assertTrue(user.check_password(password))
@@ -30,20 +45,30 @@ class ModelTests(TestCase):
         ]
 
         for email, expected in sample_email:
-            user = get_user_model().objects.create_user(email, 'password123')
+            user = create_user(email=email)
             self.assertEqual(user.email, expected)
 
     def test_new_user_without_email_raises_error(self):
         """Test that creating a user without an email raises a ValueError."""
         with self.assertRaises(ValueError):
-            get_user_model().objects.create_user('', 'password123')
+            create_user(email='')
 
     def test_create_superuser(self):
         """Test to create superuser."""
-        user = get_user_model().objects.create_superuser(
-            'test1@mail.com',
-            'password123',
-        )
+        user = create_user(admin=True)
 
         self.assertTrue(user.is_superuser)
         self.assertTrue(user.is_staff)
+
+    def test_create_recipe(self):
+        """Test creating a recipe is successful."""
+        user = create_user()
+        recipe = models.Recipe.objects.create(
+            user=user,
+            title='Sample recipe name',
+            time_minutes=5,
+            price=Decimal('5.50'),
+            description='Sample recipe description'
+        )
+
+        self.assertEqual(str(recipe), recipe.title)
